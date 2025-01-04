de1dbaaeeab743c353e06ea43d7a04d07a479dd9
Fathima Firoz <98166163+mynameisfathima@users.noreply.github.com>
2025-01-04 15:28:31 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="opTech",
+    version="1.0.0",
+    description="A tool for scanning a target URL using YAML templates.",
+    author="X",
+    url="https://github.com/mynameisfathima/PenetrationTesting/tree/check",
+    py_modules=["main"],  # Assuming your file is named main.py
+    entry_points={
+        "console_scripts": [
+            "optech=main:main",  # Maps the `optech` command to the `main()` function in `main.py`
+        ],
+    },
+    install_requires=["pyyaml"],  # Add other dependencies as needed
+)
