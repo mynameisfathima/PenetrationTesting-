e5718cb93359026da41a11a5089dc611a77a55a2
Makrihashq <147227931+Makrihashq@users.noreply.github.com>
2025-01-04 22:43:16 +0530

    Create banner.py

---
+++ banner.py
@@ -0,0 +1,34 @@
+import logging
+from updateutils import get_update_tool_callback
+from auth.pdcpauth import check_and_validate_credentials
+
+
+# Define the banner
+banner = '''
+             __________________
+  ____  ____/_  __/ ____/ ____/
+ / __ \/ __ \/ / / __/ / /
+/ /_/ / /_/ / / / /___/ /___
+\____/ .___/_/ /_____/\____/
+    /_/                          1.0.0
+'''
+
+# Show the banner to the user
+def show_banner():
+    print(f"{banner}")
+    print("\t\toptec.example.com\n")
+
+# Update nuclei binary/tool to the latest version
+def nuclei_tool_update_callback():
+    show_banner()
+    get_update_tool_callback('nuclei', '1.0.0')()
+
+# Authenticate with PDCP
+def auth_with_pdcp():
+    show_banner()
+    check_and_validate_credentials('nuclei')
+
+# Example usage
+if __name__ == "__main__":
+    auth_with_pdcp()
+    nuclei_tool_update_callback()
