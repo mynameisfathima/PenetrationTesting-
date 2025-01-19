750f59b3ae02b8482aa6c77430d9c66ebfdc191e
Fathima Firoz <98166163+mynameisfathima@users.noreply.github.com>
2025-01-19 23:03:42 +0530

    Update scanner.py

--- engine/scanner.py
+++ engine/scanner.py
@@ -1,7 +1,7 @@
 import requests  # Alternative to a browser in real lyf
 from typing import Dict, Any, List
 from .matchers import run_matchers
-
+from .scannerssl import _process_ssl_request
 
 class Scanner:
     """
@@ -18,6 +18,9 @@ class Scanner:
         Scan a single target URL with all loaded templates.
         """
         results = []
+    
+
+        
         for template in self.templates:
             template_id = template.get("id", "unknown-id")
             template_info = template.get("info", {})
@@ -38,46 +41,7 @@ class Scanner:
                         results = self._process_ssl_request(template_id, template_info, request_config, target_url)
         return results
 
-    def _process_ssl_request(self, template_id, template_info, request_config, target_url):
-        paths = request_config.get("path", []) # TODO : Strip this to just domain name in case any path exists 
-        matchers_config = request_config.get("matchers", [])
-        for path in paths:
-            url = path.replace("{{BaseURL}}", target_url).strip("/")
-            try:
-                response = requests.get(url, verify=True)
-                if response.url.startswith("https://"):
-                    return {
-                        "template_id": template_id,
-                        "name": template_info.get("name"),
-                        "author": template_info.get("author"),
-                        "severity": template_info.get("severity"),
-                        "url": url,
-                        "status_code": response.status_code,
-                        "matched": "Flase", # As a postive result
-                    }
-                else:
-                    return {
-                        "template_id": template_id,
-                        "name": template_info.get("name"),
-                        "author": template_info.get("author"),
-                        "severity": template_info.get("severity"),
-                        "url": url,
-                        "status_code": response.status_code,
-                        "matched": "True", # As a negetive result
-                    }
-            except requests.exceptions.SSLError:
-                return {
-                        "template_id": template_id,
-                        "name": template_info.get("name"),
-                        "author": template_info.get("author"),
-                        "severity": template_info.get("severity"),
-                        "url": url,
-                        "status_code": response.status_code,
-                        "matched": "True", # As a negetive result
-                    }
-            except requests.exceptions.RequestException as e:
-                print("The request was errored out ", str(e))
-
+    
 
     def _process_http_request(self, template_id, template_info, request_config, target_url):
         """
