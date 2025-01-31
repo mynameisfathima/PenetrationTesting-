ab185b8807023c8ab94d58009d47be3b042def3f
Fathima Firoz <98166163+mynameisfathima@users.noreply.github.com>
2025-01-31 22:04:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="opTech",
+    name="optec",
     version="1.0.0",
     description="A tool for scanning a target URL using YAML templates.",
     author="X",
@@ -9,7 +9,7 @@ setup(
     py_modules=["main"],  # Assuming your file is named main.py
     entry_points={
         "console_scripts": [
-            "optech=main:main",  # Maps the `optech` command to the `main()` function in `main.py`
+            "optec=main:main",  # Maps the `optech` command to the `main()` function in `main.py`
         ],
     },
     install_requires=["pyyaml"],  # Add other dependencies as needed
